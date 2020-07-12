9731cd1dd8efefea531cce0274edd2cd91897fdb
natasha <natasha.gr.costa@gmail.com>
2020-07-12 13:17:45 -0300

    instalação Chalk e sua utilização no cli.js

--- cli.js
+++ cli.js
@@ -1,25 +1,26 @@
 #!/usr/bin/env node
 
 const mdLinks = require('./index.js');
+const chalk = require('chalk');
 const path = process.argv[2];
 const options = process.argv;
 
 mdLinks(path, options)
   .then(links => {
-    if(options.includes('--stats') && options.includes('--validate')){
-      const returnStats = `Total: ${links.links} \nUnique: ${links.uniqueLinks} \nBroken: ${links.broken}`;
+    if(options.includes('--stats') && !options.includes('--validate')){
+      const returnStats = `${chalk.hex('#CFA6FF').bold('Total:')} ${chalk.bold(links.links)} \n${chalk.hex('#80C8FF').bold('Unique:')} ${chalk.bold(links.uniqueLinks)}`;
       console.log(returnStats);
-    } else if(options.includes('--stats') && !options.includes('--validate')){
-      const returnStats = `Total: ${links.links} \nUnique: ${links.uniqueLinks}`;
+    } else if(options.includes('--stats') && options.includes('--validate')){
+      const returnStats = `${chalk.hex('#CFA6FF').bold('Total:')} ${chalk.bold(links.links)} \n${chalk.hex('#80C8FF').bold('Unique:')} ${chalk.bold(links.uniqueLinks)} \n${chalk.keyword('red').bold('Broken:')} ${chalk.bold(links.broken)}`;
       console.log(returnStats);
     } else if(options.includes('--validate')){
       links.forEach(infos => {
-        const eachLink = `${infos.file}  ->     ${infos.text} ->       ${infos.href} ->       ${infos.status}: ${infos.message}`;
+        const eachLink = `${chalk.hex('#CFA6FF').bold(infos.text)} \n${chalk.hex('#80C8FF').bold(infos.href)} ${infos.status > 400 ? chalk.bold('    ->    ') + chalk.keyword('red').bold(infos.status, infos.message) : chalk.bold('    ->    ') + chalk.keyword('lawngreen').bold(infos.status, infos.message)} \n${chalk.hex('#8FFFDB').bold(infos.file)} \n`;
         console.log(eachLink);
       });
     } else {
       links.forEach(infos => {
-        const eachLink = `${infos.file}  ->     ${infos.text} ->       ${infos.href}`;
+        const eachLink = `${chalk.hex('#CFA6FF').bold(infos.text)} \n${chalk.hex('#80C8FF').bold(infos.href)} \n${chalk.hex('#8FFFDB').bold(infos.file)} \n`;
         console.log(eachLink);
       });
 
